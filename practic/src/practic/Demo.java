38bd7d53384f79121bed4e68e2519cfd6e3aae10
kseniadiogenova <ksusha-dio@yandex.ru>
2015-07-01 14:25:19 +0300

    Финальная версия

--- practic/src/practic/Demo.java
+++ practic/src/practic/Demo.java
@@ -1,83 +1,135 @@
 package practic;
 import java.awt.*;
 import java.util.*;
+import java.util.concurrent.Semaphore;
 import java.awt.event.*;
+import java.io.File;
+
 import javax.swing.*;
 
-class PaintPanel extends JPanel{
-	Insets ins;
-	PaintPanel(){
-		setSize(900, 600);
-		setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-	}
-	protected void paintComponent(Graphics g){
-		super.paintComponent(g);
-		ins = getInsets();
-		//g.drawLine(10, 20, 100, 200);
-		Graph gr = new Graph();
-		int r = gr.v[0].r;
-		String str = gr.Kruskal();
-		g.setColor(Color.BLACK);
-		for(int i = 0; i < gr.numE; i++){
-			g.drawLine(gr.e[i].from.x+r/2, gr.e[i].from.y+r/2, gr.e[i].to.x+r/2, gr.e[i].to.y+r/2);
-			String weight = Integer.toString(gr.e[i].weight);
-			g.drawString(weight, (gr.e[i].from.x + gr.e[i].to.x)/2, (gr.e[i].from.y + gr.e[i].to.y)/2);
-		}
-		g.setColor(Color.RED);
-		for(int i = 0; i < gr.numM; i++){
-			g.drawLine(gr.MFT[i].from.x+r/2, gr.MFT[i].from.y+r/2, gr.MFT[i].to.x+r/2, gr.MFT[i].to.y+r/2);
-			//g.drawString([i]);
-		}
-		g.setColor(Color.CYAN);
-		for(int i = 0; i<gr.numV; i++){
-			g.fillOval(gr.v[i].x, gr.v[i].y, gr.v[i].r, gr.v[i].r);
-		}
-		g.setColor(Color.BLACK);
-		for(int i = 0; i<gr.numV; i++){
-			g.drawString(Integer.toString(i), gr.v[i].x +r/2, gr.v[i].y+r/2);
-		}
-		g.drawString(str, 510, 350);
-		
-		
-		
-	}
-}
-
 public class Demo {
-
-	JLabel jlbl;
-	PaintPanel pp;
+	Thread mainThread;
+	JLabel step;
 	static int count;
+	Semaphore sem;
+	JFrame mainWindow;
+	JButton next;
+	Graph gr;
 	
 	Demo(){
-		JFrame jfrm = new JFrame("A Simple Application");		//создается контейнер содержащий окно
-		jfrm.setSize(1000, 700);									//задается его размер
-		jfrm.setLayout(new BorderLayout());
-		jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//чтобы закрывалася на 
-		jfrm.setVisible(true);			
-		pp = new PaintPanel();
-		jfrm.add(pp, BorderLayout.CENTER);
-		jlbl = new JLabel("This is a GUI");						//метка
-		JButton btn = new JButton("Далее");
-		//JButton file = new JButton("Выбрать файл");
-		//jfrm.add(file, BorderLayout.WEST);
+		mainWindow = new JFrame("Визуализация алгоритма Краскала");		//создается контейнер, содержащий окно
+		mainWindow.setSize(1000, 700);									//задается его размер
+		mainWindow.setLayout(new BorderLayout());						//расположение объектов на нем
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		//чтобы закрывалася на 
+		mainWindow.setVisible(true);									//становится видимым
+		
+		mainThread = Thread.currentThread();
+		
+		sem = new Semaphore(1);											//создается семафор на 1 разрешение
+		try {
+			sem.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		gr = new Graph(5,100,sem);									//создается граф
+		mainWindow.add(gr, BorderLayout.CENTER);						//добавляется на окно
+		
+		step = new JLabel("Нажмите кнопку \"Далее\" для следующего шага");	//метка, отображающая номер шага
+		
+		next = new JButton("Далее");									//оздается кнопка
+		next.addActionListener(new ActionListener(){					//задается действие при нажатии на кнопку
+			public void actionPerformed(ActionEvent ae){
+				if(gr.graphThread.isAlive()){
+				count++;												//увеличивается счетчик шагов
+				step.setText("Шаг №"+Integer.toString(count));			//записывается номер шага
+				sem.release();
+				}
+				else {
+					step.setText("Завершено!");	
+				}
+			} 
+		});
+		
 		JMenuBar menubar = new JMenuBar();
-		JMenu menu = new JMenu("Файл...");
-		JMenuItem load = new JMenuItem("Загрузить");
+		JMenu menu = new JMenu("Граф");
+		JMenuItem load = new JMenuItem("Загрузить из файла");
+        JMenuItem generate = new JMenuItem("Генерация");
+        JMenuItem show = new JMenuItem("Показать ребра");
+        menu.add(show);
 		//pp.setLayout( new BorderLayout());
 		menu.add(load);
+        menu.add(generate);
 		menubar.add(menu);
-		jfrm.setJMenuBar(menubar);
-		btn.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae){
-				count++;
-				jlbl.setText(Integer.toString(count));
+		mainWindow.setJMenuBar(menubar);
+		
+		show.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent AE){
+				JFrame edges = new JFrame("Ребра графа");
+				edges.setSize(500, 500);									//задается его размер
+				edges.setLayout(new BorderLayout());						//расположение объектов на нем
+				edges.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);		//чтобы закрывалася на 
+				edges.setVisible(true);	
+				
+				JTextArea list = new JTextArea();
+				//JLabel list = new JLabel();
+				String res = new String();
+				for(int i = 0; i < gr.numE; i++){
+					res+=" ИЗ "+gr.e[i].from.num+"     В "+gr.e[i].to.num+"   весом "+gr.e[i].weight+"\n";
+				}
+				list.setText(res);
+				list.setEditable(false);
+				//list.setText(res);
+				edges.add(list);
+				JScrollPane scroll = new JScrollPane(list);
+				edges.add(scroll);
+				
 			}
 		});
-		jfrm.add(btn, BorderLayout.SOUTH);
-		jfrm.add(jlbl, BorderLayout.NORTH);										//добавляем ее в контейнер
-		//jfrm.pack();
-								//делаем окно видимым
+		
+		load.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				FileDialog fileDialog = new FileDialog(mainWindow, "Выбор файла", FileDialog.LOAD);
+				fileDialog.show();
+				if(fileDialog.getFile()!=null){
+					count = 0;
+					step.setText("Нажмите кнопку \"Далее\" для следующего шага");
+					File file = new File(fileDialog.getFile());
+					gr.graphThread.stop();
+					sem = new Semaphore(1);
+					try {
+						sem.acquire();
+					} catch (InterruptedException e) {}
+					mainWindow.remove(gr);
+					gr = new Graph(file, sem);
+					mainWindow.add(gr);
+				}
+			}
+		});
+		
+		
+		generate.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent ae){
+				String numv =JOptionPane.showInputDialog(mainWindow,"Введите число вершин:","Выбор числа вершин", JOptionPane.PLAIN_MESSAGE);
+				String proc = JOptionPane.showInputDialog(mainWindow,"Введите % ребер от 0 до 100:","Выбор % ребер", JOptionPane.PLAIN_MESSAGE);
+				if(numv!=null &&proc != null ){
+					count = 0;
+					step.setText("Нажмите кнопку \"Далее\" для следующего шага");
+					gr.graphThread.stop();
+					sem = new Semaphore(1);
+					try {
+						sem.acquire();
+					} catch (InterruptedException e) {}
+					mainWindow.remove(gr);
+					gr = new Graph(Integer.parseInt(numv),Integer.parseInt(proc), sem);
+					mainWindow.add(gr);
+				}
+			}
+		});
+		
+		mainWindow.add(next, BorderLayout.SOUTH);
+		mainWindow.add(step, BorderLayout.NORTH);										//добавляем ее в контейнер
+
 	}
 	
 	public static void main(String[] args) {
--- practic/src/practic/Graph.java
+++ practic/src/practic/Graph.java
@@ -1,15 +1,22 @@
 package practic;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.io.*;
-import java.math.*;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.*;
 
 class Vertex{
 	public int x, y;	//координаты вершины
-	public int r;		//радиус круга
 	public int num;
-	public Vertex(int x, int y, int r,int num){
+	public Vertex(int x, int y, int num){
 		this.x = x;
 		this.y = y;
-		this.r = r;
 		this.num = num;
 	}
 }
@@ -25,51 +32,142 @@ class Edge{
 	}
 }
 
-public class Graph {
+public class Graph extends JPanel implements Runnable{
 	public int numV;
+	Semaphore sem;
 	public int numE;
 	public int numM;
+	int currentEdge;
+	JLabel doings;
+	public Thread graphThread;
+    public int r;
 	public Vertex v[];
 	public Edge e[];
 	public Edge MFT[];
-	public Graph(){
-		try {
-			
-			FileInputStream fs = new FileInputStream("in.txt");
-			InputStreamReader is = new InputStreamReader(fs);
-			BufferedReader br = new BufferedReader(is);
-			String str = br.readLine();
-			numV = Integer.parseInt(str);
-			MFT = new Edge[numV-1];
-			v = new Vertex[numV];
-			double angle = 6.2831853/numV;
-			double nextangle = 0;
-			int R = 200;
-			int r = 50;
-			int CentrX=250, CentrY=250;
-			int x, y;
-			for(int i = 0; i<numV; i++){
-				x = (int) (CentrX + Math.cos(nextangle)*R);
-				y = (int) (CentrY + Math.sin(nextangle)*R);
-				nextangle += angle;
-				v[i] = new Vertex(x, y, r, i);
-			}
-			str = br.readLine();
-			numE = Integer.parseInt(str);
-			e = new Edge[numE];
-			for(int i = 0; i<numE; i++){
-				str = br.readLine();
-				int from = Integer.parseInt(str);
-				str = br.readLine();
-				int to = Integer.parseInt(str);
-				str = br.readLine();
-				int weight = Integer.parseInt(str);
-				e[i] = new Edge(v[from], v[to], weight);
-			}
-			br.close();
+	public void run(){
+		doings.setText("");
+		repaint();
+		Kruskal();
+		
+	}
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+        //настройка шрифта
+		g.setFont(g.getFont().deriveFont((float)r/2));
+		//отрисовка всех ребер графа
+		g.setColor(Color.BLACK);
+		for(int i = 0; i < numE; i++){
+			g.drawLine(e[i].from.x+r/2, e[i].from.y+r/2,e[i].to.x+r/2,e[i].to.y+r/2);        
+		}
+		//отрисовка ребер мод
+		g.setColor(Color.RED);
+		for(int i = 0; i < numM; i++){   
+			g.drawLine(MFT[i].from.x+r/2, MFT[i].from.y+r/2, MFT[i].to.x+r/2, MFT[i].to.y+r/2);
+		}
+		//отрисовка весов ребер
+		g.setColor(Color.BLUE);
+		for(int i = 0; i<numE; i++){
+			String weight = Integer.toString(e[i].weight);
+			g.drawString(weight, (e[i].from.x + e[i].to.x +r)/2, (e[i].from.y + e[i].to.y +r)/2);
+		}
+		//отрисовка вершин
+		g.setColor(Color.CYAN);
+		for(int i = 0; i<numV; i++){
+			g.fillOval(v[i].x,v[i].y, r, r);
+		}
+		if(currentEdge!=-1){
+			g.drawLine(e[currentEdge].from.x + r/2, e[currentEdge].from.y + r/2, e[currentEdge].to.x + r/2, e[currentEdge].to.y + r/2);
 			
 		}
-		catch(IOException e){};
+		//отрисовка номеров вершин
+		g.setColor(Color.BLACK);
+		for(int i = 0; i<numV; i++){
+			g.drawString(Integer.toString(i), v[i].x + 3*r/8, v[i].y + 2*r/3);
+		}
+	}
+	private void bothConstr(){
+		currentEdge = -1;
+		doings = new JLabel("");					//создание метки под вывод последовательности действий
+		graphThread = new Thread(this);
+		this.setLayout(new BorderLayout());
+		this.add(doings,BorderLayout.NORTH);
+		double angle = 6.2831853/numV;				//вычиление расположения вершин
+        double nextangle = 0;
+        int R = 200;
+        r = (int)(6.28*R)/(4*numV);
+        if(r>50)r = 50;
+        if (r < 20){
+            //добавить скролл бар
+            r = 20;
+        }
+        int CentrX=250, CentrY=250;
+        int x, y;
+        for(int i = 0; i<numV; i++){
+            x = (int) (CentrX + Math.cos(nextangle)*R);
+            y = (int) (CentrY + Math.sin(nextangle)*R);
+            nextangle += angle;
+            v[i] = new Vertex(x, y, i);
+        }
+	}
+	public Graph(int numvert, int proc, Semaphore sem){
+		//инициализация графа по заданым параметрам
+		int max = numvert*(numvert-1)/2;			//максимально возможное число верин в графе
+		int must = (int) (proc*numvert*(numvert-1)/100)/2;			//необходимое нам число вершин
+		int counter = 0;							//счетчик для массива вершин
+		numV = numvert;								//инициализация полей графа
+		numE = must;
+		e = new Edge[must];							//создание массивов под вершины, ребра и мод
+		v = new Vertex[numvert];
+		MFT = new Edge[numV-1];
+		
+		this.sem = sem;
+		
+		bothConstr();
+        
+		Random rand = new Random(System.currentTimeMillis());			//рандомные значения
+		Random weightrand = new Random(System.currentTimeMillis());
+		
+		for(int i = 0; i < numvert && must > 0; i++){					//идем по вершинам, от которых можно провести ребро
+			for(int j = i+1; j < numvert && must > 0; j++){				//к которым можно его провести
+				if(rand.nextInt(max)< must){							//ставим ребро между i и j с вероятностью must/max
+					must--;												//если ставим, уменьшаем оставшееся число ребер
+					e[counter] = new Edge(v[i],v[j],weightrand.nextInt(max)+1);	//ставим ребро
+					counter++;											//увеличиваем счетчик массива
+				}
+				max--;													//уменьшаем максимально возможное оставшееся число ребер
+			}
+		}
+		graphThread.start();
+	}
+	public Graph(File file, Semaphore sem){
+            Scanner in;
+            try {
+                in = new Scanner(file);
+               
+                
+                
+                this.sem = sem;
+                doings = new JLabel("");
+                
+                numV = in.nextInt();
+                
+                MFT = new Edge[numV-1];
+                v = new Vertex[numV];
+                numE = in.nextInt();
+                bothConstr();
+                e = new Edge[numE];
+                for(int i = 0; i<numE; i++){
+                    int from = in.nextInt();
+                    int to = in.nextInt();
+                    int weight = in.nextInt();
+                                    e[i] = new Edge(v[from], v[to], weight);
+                            }
+                in.close();
+                graphThread.start();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Graph.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
 	}
 	private void sortEdges(){
 		for(int i = 0; i <numE-1; i++){
@@ -82,9 +180,10 @@ public class Graph {
 			}
 		}
 	}
-	public String Kruskal(){
+	public void Kruskal(){
 		sortEdges();
-		String result = new String();
+		String result = "Последовательность добавления: ";
+		doings.setText(result);
 		boolean unions[][]  = new boolean[numV][numV];
 		int counter = 0;
 		for (int j = 0; j< numV; j++){
@@ -92,6 +191,22 @@ public class Graph {
 		}
 		
 		for(int i = 0; i<numE && (counter < numV-1); i++){
+			
+			try {
+				sem.acquire();
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			currentEdge = i;
+			doings.setText(result);
+			this.repaint();
+			try {
+				graphThread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			Edge help = e[i];
 			int from = e[i].from.num;
 			int to = e[i].to.num;
@@ -122,15 +237,22 @@ public class Graph {
 					if(unions[j][to]){
 						unions[j][from] = true;
 					}
-					//unions[j][from] = unions[j][to] = unions[j][from] || unions[j][to];
 				}
 				MFT[counter] = help;
 				counter++;
 				result+=help.from.num+"-> "+help.to.num+" ;";
+				doings.setText(result);
+				currentEdge = -1;
+				this.repaint();
+				numM = counter;
+			}
+			else{
+				doings.setText("ЦИКЛ!");
+				currentEdge = -1;
+				this.repaint();
 			}
 		}
-		numM = counter;
-		return result;
+		
 		
 	}
 }
